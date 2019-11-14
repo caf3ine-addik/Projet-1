53243e486b78aa6e9eef2ae81589bc519ea281b6
David <57725336+caf3ine-addik@users.noreply.github.com>
2019-11-14 16:43:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import argparse
 
 
 def analyser_commande():
-    parser = arparse.ArgumentParser(description= "Jeu Quoridor - Phase 1")
+    parser = argparse.ArgumentParser(description = "Jeu Quoridor - Phase 1")
     parser.add_argument('idul', type=str, help='IDUL du joeur.')
     parser.add_argument('-l', '--lister', metavar='', help= 'Lister les identifiants de vos 20 dernières parties.')
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 if __name__ == "main":
     analyser_commande()
