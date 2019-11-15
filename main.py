b12e4d5991f67b428404ffeac968f183d31751e9
David <57725336+caf3ine-addik@users.noreply.github.com>
2019-11-14 23:13:24 -0500

    test sur fnc 1

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import argparse
 import json
 
 
+state_0 = {
+    "joueurs": [
+        {"nom": "idul", "murs": 7, "pos": [5, 5]}, 
+        {"nom": "automate", "murs": 3, "pos": [8, 6]}
+    ], 
+    "murs": {
+        "horizontaux": [[4, 4], [2, 6], [3, 8], [5, 8], [7, 8]], 
+        "verticaux": [[6, 2], [4, 4], [2, 6], [7, 5], [7, 7]]
+    }
+}
+
 def analyser_commande():
     parser = argparse.ArgumentParser(description = "Jeu Quoridor - Phase 1")
     parser.add_argument('idul', type=str, help='IDUL du joeur.')
@@ -15,8 +26,7 @@ if __name__ == "main":
 
 #Fonction 2
 def afficher_damier_ascii(state_0):
-    json.loads(state_0)
-    haut = f'Légende: 1={state_0["joueurs"][0]['nom']}, 2=automate\n'
+    haut = f'Légende: 1={state_0["joueurs"][0]["nom"]}, 2=automate\n'
     haut +='   -----------------------------------\n'
     bas = '--|-----------------------------------\n'
     bas +='  | 1   2   3   4   5   6   7   8   9'
